87a652b93d9ce3cc2b859ad006a7d22379107f45
AlexForw <lmityushkin@mail.ru>
2022-04-14 00:10:25 +0300

    feat: check the API work

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
+import { useEffect } from "react";
 
 
 const API = 'http://www.omdbapi.com?apikey=47daf436'
 
-function App() {
+const App = () =>{
+
+  const searchFilm = async (target) =>{
+    const resp = await fetch(`${API}&s=${target}`)
+    const data = await resp.json()
+  }
+
+  useEffect(()=>{
+    searchFilm('AnyFilm')
+  }, [])
+
   return (
     <div className="App">
       
